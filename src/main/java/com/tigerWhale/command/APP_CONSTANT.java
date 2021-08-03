e23d324ef32670d35e4140f44512d4c527ddcbab
MF1204 <kstydevil@gmail.com>
2021-08-04 00:14:55 +0900

    6th

--- src/main/java/com/tigerWhale/command/APP_CONSTANT.java
+++ src/main/java/com/tigerWhale/command/APP_CONSTANT.java
@@ -2,6 +2,6 @@ package com.tigerWhale.command;
 
 public class APP_CONSTANT {
 
-	public static final String UPLOAD_PATH = "/var/upload";
+	public static final String UPLOAD_PATH = "/var/upload/";
 	
 }
